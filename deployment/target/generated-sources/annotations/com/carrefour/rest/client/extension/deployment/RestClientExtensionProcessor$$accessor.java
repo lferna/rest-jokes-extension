ac2b06d8d620af26ea1ac74db097687135fb1515
Luis Fernandez Palacio <luis.fernandez-palacio@capgemini.com>
2021-05-25 16:13:02 +0200

    delete

--- deployment/target/generated-sources/annotations/com/carrefour/rest/client/extension/deployment/RestClientExtensionProcessor$$accessor.java
+++
@@ -1,7 +0,0 @@
-package com.carrefour.rest.client.extension.deployment;
-public final class RestClientExtensionProcessor$$accessor {
-    private RestClientExtensionProcessor$$accessor() {}
-    public static Object construct() {
-        return new RestClientExtensionProcessor();
-    }
-}
--- deployment/target/generated-sources/annotations/com/jokes/rest/client/extension/deployment/RestClientExtensionProcessor$$accessor.java
+++
@@ -1,7 +0,0 @@
-package com.jokes.rest.client.extension.deployment;
-public final class RestClientExtensionProcessor$$accessor {
-    private RestClientExtensionProcessor$$accessor() {}
-    public static Object construct() {
-        return new RestClientExtensionProcessor();
-    }
-}
